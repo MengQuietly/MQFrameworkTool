c5ce64b6f20feb92faa65dc77028a8d70c453d09
mengmeng <wenjing@hiqianbao.com>
2018-07-11 18:19:53 +0800

    项目初始化

---
+++ MQFrameworkTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MQFrameworkTool
+//
+//  Created by mengJing on 2018/7/11.
+//  Copyright © 2018年 mengJing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
